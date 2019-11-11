4fbc6cb945b59a3d62932777295ad4c7cb2ef272
Sammylukorito23 <41210924+Sammylukorito23@users.noreply.github.com>
2019-11-11 13:57:25 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MainStackNav from './Pages/StackNavs/StackNav';export default class App extends React.Component {
+  render() {
+    return  <MainStackNav />
+  }
+}
+    
